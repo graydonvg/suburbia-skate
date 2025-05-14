9dc1c6a7a1b00d75072d97c5921ac75dcfce52b1
GraydonvG <graydonvg@gmail.com>
2025-05-14 12:05:31 +0200

    add links to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,13 @@
 import Link from "next/link";
 import { ButtonLink } from "./ButtonLink";
 import { Logo } from "./Logo";
+import { createClient } from "@/prismicio";
+import { PrismicNextLink } from "@prismicio/next";
 
-export function Header() {
+export async function Header() {
+  const client = createClient();
+  const settings = await client.getSingle("settings");
+
   return (
     <header className="header absolute inset-x-0 top-0 z-50 ~h-32/48 ~p-4/6 md:h-32">
       <div className="mx-auto grid w-full max-w-6xl grid-cols-[auto,auto] items-center gap-6 md:grid-cols-[1fr,auto,1fr]">
@@ -14,10 +19,14 @@ export function Header() {
           className="col-span-full row-start-2 md:col-span-1 md:col-start-2 md:row-start-1"
         >
           <ul className="flex flex-wrap items-center justify-center gap-8">
-            <li>Boards</li>
+            {settings.data.navigation.map((item) => (
+              <li key={item.link.text}>
+                <PrismicNextLink field={item.link} className="~text-lg/xl" />
+              </li>
+            ))}
           </ul>
         </nav>
-        <div className="place-self-end">
+        <div className="justify-self-end">
           <ButtonLink href="" icon="cart" color="purple" aria-label="Cart (1)">
             <span className="md:hidden">(1)</span>
             <span className="hidden md:inline">Cart(1)</span>
